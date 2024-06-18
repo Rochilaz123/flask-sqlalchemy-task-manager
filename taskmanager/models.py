1e54104fd3042a4e08cfeae0d7706d4231eb0886
Rachel Lazarus <rochilazarus@gmail.com>
2024-06-18 19:21:56 +0000

    User can create and read tasks

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -8,9 +8,8 @@ class Category(db.Model):
     tasks = db.relationship("Task", backref="category", cascade="all, delete", lazy=True)
 
     def __repr__(self):
-        #__repr__ to represent itself in the form of a string
+        # __repr__ to represent itself in the form of a string
         return self.category_name
-
 
 
 class Task(db.Model):
@@ -20,10 +19,10 @@ class Task(db.Model):
     task_description = db.Column(db.Text, nullable=False)
     is_urgent = db.Column(db.Boolean, default=False, nullable=False)
     due_date = db.Column(db.Date, nullable=False)
-    cstegory_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelete="CASCADE"), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelete="CASCADE"), nullable=False)
 
     def __repr__(self):
-        #__repr__ to represent itself in the form of a string
+        # __repr__ to represent itself in the form of a string
         return "#{0} - Task: {1} | Urgent: {2}".format(
             self.id, self.task_name, self.is_urgent
         )
